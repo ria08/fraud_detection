91736a72455fa59cdbe4a2e4fecfdb9967038de5
ria08 <singhria.0829@gmail.com>
2025-05-16 17:51:11 +0530

    added a func

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
 print("adding all the data processing code related to fraud detection")
+
+a=5
+b=7
+
+def sum(a,b):
+    c= a+b 
+    return a
+print("added new function of sum which is not correct")
